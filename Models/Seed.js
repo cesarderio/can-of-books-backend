55752c5a082262e3ff46a4da95712cd0cda22c3b
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-31 15:43:02 -0700

    creating a model from the schema

---
+++ Models/Seed.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+mongoose.connect(process.env.DB_URL);
+
+const Book = require('./models/Book.js');
+
+async function seed() {
+  // **name: {type: String, required: true},
+  // **color: {type: String, required: true},
+  // **spayNeuter: {type: Boolean, required: true},
+  // **location: {type: String, required: true}
+
+  await Book.create({
+    title: 'Pat the cat',
+    description: 'Pat the cat is not a rat',
+    status: 'available'
+  });
+
+  console.log('Pat the cat was created');
+
+  await Book.create({
+    title: 'Charles the cat',
+    description: 'Charles the cat',
+    status: 'available'
+  });
+
+  console.log('Charles the cat was created');
+
+  await Book.create({
+    title: 'Ray the cat',
+    description: 'Ray the cat',
+    status: 'not available'
+  });
+
+  console.log('Ray the cat was created');
+
+  mongoose.disconnect();
+}
+
+seed();
--- server.js
+++ server.js
@@ -10,10 +10,33 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
+app.get('/', (request, response)=>{
+  response.status(200).send('Welcome to my server');
+});
+
+const Book = require('./Models/Book');
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+
+
+
+
+
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
 })
 
+app.use((error, request, response, next)=>{
+  response.status(404).send('Not available');
+})
+
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
